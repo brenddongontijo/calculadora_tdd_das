5be82b9791acef5e9a654dd9306bdd954ad0ed20
Brenddon Gontijo <brenddongontijo@msn.com>
2016-04-07 09:06:37 -0300

    Add minus function and tests

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 class ClassCalculator:
 	def sum(self, firstNumber, secondNumber):
 		return firstNumber + secondNumber
+
+	def minus(self, firstNumber, secondNumber):
+		return firstNumber - secondNumber
--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,7 @@ from calculator import ClassCalculator
 def test_sum():
 	calculator = ClassCalculator()
 	assert calculator.sum(5,5) == 10
+
+def test_minus():
+	calculator = ClassCalculator()
+	assert calculator.minus(5,5) == 0
